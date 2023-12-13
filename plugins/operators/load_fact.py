c20e54699411c04bb831d38b4e95dc09cf00488b
CptCapt <150163847+CptCapt@users.noreply.github.com>
2023-12-13 16:22:44 +0100

    Update load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -3,20 +3,37 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
+    """
+    Loads fact table in Redshift from data in staging table(s)
+    
+    redshift_conn_id: Redshift connection ID
+    table: Target table in Redshift to load
+    sql: SQL query for getting data to load into target table
+    """
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 table = "",
+                 sql = "",        
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql = sql
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        
+        self.log.info("Getting redshift credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Loading data into fact table in Redshift")
+        table_insert_sql = f"""
+            INSERT INTO {self.table}
+            {self.sql}"""
+        
+        redshift_hook.run(table_insert_sql)
+        self.log.info(f"Data sucessfully loaded into {self.table}")
